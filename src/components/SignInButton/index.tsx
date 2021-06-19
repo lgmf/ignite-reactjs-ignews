c907baa34e85037ef2b1beb47778368ab2615072
lgmf <lgm.ferrari@gmail.com>
2021-06-19 17:06:22 -0300

    feat: add github auth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,23 +1,32 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+import { signIn, useSession, signOut } from "next-auth/client";
 
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const [session] = useSession();
 
-  if (isUserLoggedIn) {
+  if (session) {
     return (
-      <button type="button" className={styles.signInButton}>
+      <button
+        type="button"
+        className={styles.signInButton}
+        onClick={() => signOut()}
+      >
         <FaGithub className={styles.leftIcon} color="#04d361" />
-        Luiz Ferrari
-        <FiX className={styles.rightIcon} color="var(--gray-400)"/>
+        {session.user.name}
+        <FiX className={styles.rightIcon} color="var(--gray-400)" />
       </button>
     );
   }
 
   return (
-    <button type="button" className={styles.signInButton}>
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn("github")}
+    >
       <FaGithub className={styles.leftIcon} color="var(--yellow-500)" />
       Sign In with Github
     </button>
